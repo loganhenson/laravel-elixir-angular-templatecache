f36dc2e05017f253e940cfecfa55f9a23d658be6
Logan Henson <logan@loganhenson.com>
2014-11-01 12:20:39 -0500

    added iffy param, updated readme, version bump

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 var elixir = require('laravel-elixir');
 var gulp = require("gulp");
 var templateCache = require('gulp-angular-templatecache');
+var insert = require('gulp-insert');
 
-elixir.extend("angulartemplatecache", function(module, from, to) {
+elixir.extend("angulartemplatecache", function(module, from, to, wrap) {
 
-    gulp.task("angulartemplatecache", function() {
-        gulp.src(from)
-            .pipe(templateCache({module: module}))
-            .pipe(gulp.dest(to));
-    });
+    if(wrap){
+
+        gulp.task("angulartemplatecache", function() {
+            gulp.src(from)
+                .pipe(templateCache({module: module}))
+                .pipe(insert.wrap('(function(angular) {', '})(angular);'))
+                .pipe(gulp.dest(to));
+        });
+
+    } else{
+
+        gulp.task("angulartemplatecache", function() {
+            gulp.src(from)
+                .pipe(templateCache({module: module}))
+                .pipe(gulp.dest(to));
+        });
+
+    }
 
     this.registerWatcher("angulartemplatecache", from);
     return this.queueTask("angulartemplatecache");
--- readme.md
+++ readme.md
@@ -1,23 +1,13 @@
 ## Usage
 
-This is a simple wrapper around Laravel Elixir. Add it to your Elixir-enhanced Gulpfile, like so:
-
 ```
 var elixir = require('laravel-elixir');
 
 require('laravel-elixir-angular-templatecache');
 
 elixir(function(mix) {
-   mix.angulartemplatecache('app', 'resources/assets/js/*/**.html', 'public');
+   mix.angulartemplatecache('app', 'resources/assets/js/**/*.html', 'public');
 });
 ```
 
-This will scan your `resources/assets/js` directory for all .html files, and put a file named `templates.js` in the public directory
-
-Make sure you include this file after your angular module in instantiated, as it creates a run block.
-
-If you'd like to output to a different directory than `public/`..
-
-```
-mix.angulartemplatecache('app', 'resources/assets/js/*/**.html', "public/foo/bar/");
-```
+To wrap in a IFFY with angular injected, pass true as the 4th argument.
